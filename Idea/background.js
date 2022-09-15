4d07bb36ed08f6f32326289b0ca9fddde26b0e55
mohammadfg <mohamadfg822@gmail.com>
2022-09-15 06:43:27 -0500

    delete idea

--- Idea/background.js
+++
@@ -1,9 +0,0 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) { 
-    if (tab.url && tab.url.startsWith('https://cafebazaar.ir/app/') && changeInfo.status == 'complete') {
-        console.log(`injecting script: `, tab.url)
-        chrome.scripting.executeScript({
-            files: ['injectScript.js'],
-            target: {tabId: tab.id}
-        })
-    }
-})
