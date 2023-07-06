f9873ab581ac7e0598d475110dda89f7331b0d24
mohammadfg <mohamadfg822@gmail.com>
2023-07-06 09:05:54 -0500

    rewrite toggle theme

--- Clients/Extension/src/Components/Advertising.tsx
+++ Clients/Extension/src/Components/Advertising.tsx
@@ -1,11 +1,30 @@
-export default function Advertising() {
+interface Advertising {
+  link: string;
+  title: string;
+  counter:number
+}
+export default function Advertising({ title, link ,counter }:Advertising) {
+  function handleAd() {}
   return (
-    <div class="AD">
-      <button id="close" disabled>
-        O
+    <div className="relative">
+      <button
+        className="absolute right-3 font-bold bg-[#00000085] px-2 mt-2 rounded-md text-red-600"
+        onClick={handleAd}
+      >
+        X
       </button>
-      <img src="./icon/129.png" loading="lazy" alt="AD" />
-      <p id="redirecAD">🌐 رفتن به صفحه موردنظر</p>
+      <img
+        src="https://unsplash.it/288/470"
+        loading="lazy"
+        alt="Adveretising"
+      />
+      <a
+        className="absolute text-center bottom-0 bg-[#ffffff29] backdrop-blur-md w-full rounded-t-lg p-3"
+        href="#"
+        target="_blank"
+      >
+        🌐 رفتن به صفحه موردنظر
+      </a>
     </div>
   );
 }
--- Clients/Extension/src/index.tsx
+++ Clients/Extension/src/index.tsx
@@ -1,21 +1,23 @@
-import { render } from 'preact';
-import { useState } from 'preact/hooks';
-import viteLogo from '/vite.svg';
-import './style.css';
+import { render } from "preact";
+import { useState } from "preact/hooks";
+import "./style.css";
+import Advertising from "./Components/Advertising";
 
 export function App() {
-	const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} class="logo" alt="Vite logo" />
-				</a>
-			</div>
-			<h1 className='bg-red-700'>Vite + Preact</h1>
-		</>
-	);
+  return (
+    <div>
+      <div className="w-72 dark:bg-[#0a1722] max-h-[470px]">
+        {/* <Advertising link="" title="" counter={0} /> transform: translate(49%,50%);*/}
+        <div className="relative w-5 h-5 rounded-[50%] bg-white cursor-pointer m-2 transition-all duration-[0.5s] before:transition-all before:duration-[0.5s] before:content-[''] before:w-[50%] before:rounded-full before:h-[50%] before:bg-[#ff8d00] before:z-[1] before:absolute before:transform before:translate-y-1/2 before:translate-x-1/2 after:content-[''] dark:before:bg-[#3f3f4c] dark:before:w-1/2 dark:before:h-3 dark:before:transform dark:before:translate-x-[4%] dark:before:translate-y-[13%] dark:before:rotate-[134deg] after:z-[2] after:absolute after:w-full after:h-full after:rounded-[50%] after:border-dotted dark:after:border-solid after:border-[4px] after:box-border after:border-amber-500 after:transition-all after:duration-[0.5s]  dark:after:border-[#3f3f4c] dark:after:shadow-[0_0_0_2px_#cdcdcd]"></div>
+        <div className="toggle">
+          <input type="checkbox" />
+          <label></label>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-render(<App />, document.getElementById('app'));
+render(<App />, document.getElementById("app"));
